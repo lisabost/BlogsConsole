f0c24fe49077ac929c4bbd88c6bdb3bc7177178f
Lisa Bost <bost.lisam@gmail.com>
2021-03-29 13:35:03 -0500

    Created user options Implemented option 1 and 2

--- Program.cs
+++ Program.cs
@@ -14,30 +14,57 @@ namespace BlogsConsole
         {
             logger.Info("Program started");
 
-            try
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("** Welcome to the Blog Database System **");
+            Console.WriteLine("*****************************************");
+            Console.WriteLine("Choose an Option");
+            Console.WriteLine("1) Display all Blogs");
+            Console.WriteLine("2) Add Blog");
+            Console.WriteLine("3) Create Post");
+            Console.WriteLine("3) Display Posts");
+
+            var choice = Console.ReadLine();
+            var db = new BloggingContext();
+
+            if (choice == "1")
             {
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
-
-                var blog = new Blog { Name = name };
-
-                var db = new BloggingContext();
-                db.AddBlog(blog);
-                logger.Info("Blog added - {name}", name);
-
-                // Display all Blogs from the database
-                var query = db.Blogs.OrderBy(b => b.Name);
-
-                Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                logger.Info("User choice: 1 - Display all Blogs");
+                try
                 {
-                    Console.WriteLine(item.Name);
+                    // Display all Blogs from the database
+                    var query = db.Blogs.OrderBy(b => b.Name);
+
+                    Console.WriteLine("All blogs in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Name);
+                    }
                 }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
+                }
+
             }
-            catch (Exception ex)
+            else if (choice == "2")
             {
-                logger.Error(ex.Message);
+                logger.Info("User choice: 2 - Add Blogs");
+                try
+                {
+                    // Create and save a new Blog
+                    Console.Write("Enter a name for a new Blog: ");
+                    var name = Console.ReadLine();
+
+                    var blog = new Blog { Name = name };
+
+                    db.AddBlog(blog);
+                    logger.Info("Blog added - {name}", name);
+
+                }
+                catch (Exception ex)
+                {
+                    logger.Error(ex.Message);
+                }
             }
 
             logger.Info("Program ended");
